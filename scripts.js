9d724a052bd46606966dbc3c7babc73806af836c
thiago kuperman <kupermanthiago@gmail.com>
2021-10-25 00:07:34 -0300

    tarea 2 JS

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let boca = prompt("ingrese VAMOS BOCA");
+let boca2 = prompt("diga por que club de futbol simpatiza");
+
+if((boca == "VAMOS BOCA") && (boca2 == "boca")){
+    console.log("FELICITACIONES USTED ES DE BOCA!!");
+}
+else{
+    console.log("usted es amargo. VAMOS BOCAAA!!!!")
+}

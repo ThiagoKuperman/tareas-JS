32d3a12c945454ed82b290e4d714abde309a0d1d
thiago kuperman <kupermanthiago@gmail.com>
2021-11-03 14:37:53 -0300

    tarea 4

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//tarea 3
 function loopDePares (numero){
     for(let i=1; i<=100; i++){
         console.log(i)
@@ -7,3 +8,18 @@ function loopDePares (numero){
     }
 }
 //esta funcion te retorna todos los numeros del 1 al 100, y te dice cuales de esos sumados a la variable que agregaste es par
+
+//tarea 4
+
+function middleCharacter(texto){
+    let caracteres = texto.length;
+    let division = caracteres/2 ;
+    if(caracteres % 2 == 0){
+        return(texto [division - 1] + texto[division]);
+    }
+    else{
+        return(texto[division - 0.5]);
+    }
+}
+
+//esta funcion retorna la letra del medio de la palabra que pasen como parametro. Si la palabra no tiene letra central retorna las 2 letras del medio
